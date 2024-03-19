cfd0b1e2cbd8b3bacb26ee6c88787622687fd754
Kat-Erina <nalchevanidzekaterina@gmail.com>
2024-03-20 00:20:44 +0400

    working on calculator functionality

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+
+function App() {
+  let [total, setTotal] = useState("");
+  let [val, setVal] = useState(0);
+  let [arr, setArr] = useState([]);
+
+  // const array = ["1", "2", "+", "2", "-", "0", "*"];
+
+  // const numArray = array.map((el) => {
+  //   if (!parseInt(el)) {
+  //     return el;
+  //   }
+  //   return parseInt(el);
+  // });
+  // console.log(numArray);
+
+  // function getValue(param) {
+  //   console.log(param);
+  // }
+
+  console.log(total);
+  console.log(arr);
+  return (
+    <>
+      <div>
+        <button
+          value="1"
+          onClick={(e) => {
+            let newarr = [...arr, e.target.value];
+            setArr(newarr);
+          }}
+        >
+          1
+        </button>
+        <button
+          value="3"
+          onClick={(e) => {
+            // getValue(e.target.value);
+            // setTotal((prev) => prev + e.target.value);
+            // console.log(total);
+            let newarr = [...arr, e.target.value];
+            setArr(newarr);
+          }}
+        >
+          3
+        </button>
+        <button
+          value="+"
+          onClick={(e) => {
+            // getValue(e.target.value);
+            // setTotal((prev) => prev + e.target.value);
+            // console.log(total);
+            let newarr = [...arr, e.target.value];
+            setArr(newarr);
+          }}
+        >
+          +
+        </button>
+        <button
+          value="2"
+          onClick={(e) => {
+            // getValue(e.target.value);
+            // setTotal((prev) => prev + e.target.value);
+            // console.log(total);
+            let newarr = [...arr, e.target.value];
+            setArr(newarr);
+          }}
+        >
+          2
+        </button>
+        <button
+          value="*"
+          onClick={(e) => {
+            // getValue(e.target.value);
+            // setTotal((prev) => prev + e.target.value);
+            // console.log(total);
+            let newarr = [...arr, e.target.value];
+            setArr(newarr);
+          }}
+        >
+          *
+        </button>
+        <button
+          value="="
+          onClick={(e) => {
+            console.log(arr);
+          }}
+        >
+          =
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
